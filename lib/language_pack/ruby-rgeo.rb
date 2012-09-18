7fb64974e243d486faf97c32406293dd8a827821
Chris Apolzon <apolzon@gmail.com>
2012-09-18 12:57:24 -0700

    correct paths to proj and rgeo dirs

--- lib/language_pack/ruby-rgeo.rb
+++ lib/language_pack/ruby-rgeo.rb
@@ -44,7 +44,7 @@ class LanguagePack::Ruby < LanguagePack::Base
       install_rgeo_binary(name, version)
     end
     binary_names.each {|name| pipe "ls #{pwd}/bin/#{name}" }
-    ENV['BUNDLE_BUILD__RGEO'] = binary_names.map{|name| "--with-#{name}-dir=#{pwd}/bin/#{name}"}.join(' ')
+    ENV['BUNDLE_BUILD__RGEO'] = binary_names.map{|name| "--with-#{name}-dir=/app/bin/#{name}"}.join(' ')
     puts ENV.to_hash.inspect
     orig_compile
   end
